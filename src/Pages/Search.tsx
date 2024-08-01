c276ec0039b1801b92fe8395ba37306a8c1f062b
Klaudia Korbiel <klaudiakorbiel@Air-Klaudia.localdomain>
2024-08-01 22:12:13 +0200

    Search fixes

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Products from '../Components/Products/Products'
+import { useLocation } from 'react-router-dom';
+import Item from '../Components/Items/Item';
+
+interface Product {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  type: string;
+}
+
 
 export const Search = () => {
+  const location = useLocation();
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState('');
+  const [categoryProducts, setCategoryProducts] = useState<Product[]>([]);
+
+
+  const getSearchProducts = async (search: string) => {
+    const response = await fetch(`http://localhost:8888/?q=${search}`, {
+      method: 'get'
+    });
+    return response.json();
+  };
+
+  useEffect(() => {
+
+    const urlParts = location.pathname.split('/');
+    const searchName = urlParts[urlParts.length - 1];
+    setSearch(searchName);
+
+
+    const fetchProduct = async () => {
+      const products = await getSearchProducts(searchName);
+      setCategoryProducts(products);
+    };
+    fetchProduct();
+
+    return () => { };
+  }, []);
   return (
-    <Products />    
+    <div>
+      <h2>{search}</h2>
+      <div className='recommended-products'>
+        <div className='recommended-product'>
+          {categoryProducts.map((item: Product, i: number) => (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              price={<span>{item.price} zł</span>}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+
   )
 }
 
